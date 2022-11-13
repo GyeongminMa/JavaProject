555450e1e8bb4fb6aa4ef07e3d5a852db15b9bed
mgyeo <mgyeo@DESKTOP-OAQRT6U>
2022-11-13 16:44:52 +0900

    07 Homework

---
+++ chapter07homework/src/homework03/Culture.java
@@ -0,0 +1,33 @@
+package homework03;
+
+public class Culture {
+	String name;
+	int diNumber;
+	int actNumber;
+	int	attendance;
+	int tScore;
+	
+	public Culture(String name,int diNumber,int actNumber, int attendance, int tScore) {
+		this.name=name;
+		this.diNumber = diNumber;
+		this. actNumber = actNumber;
+		this. attendance = attendance;
+		this.tScore = tScore;
+		
+		//메소드
+		public void setTotalScore(int score) {
+			int tScore(int[] values) {
+				int tScore = 0;
+				for(int i=0; i<values.length; i++) {
+					sum += values[i];
+				}
+				return = sum;
+			}
+		};
+		
+		
+		
+		
+		
+		}
+}
